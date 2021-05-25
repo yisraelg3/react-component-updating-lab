f0fa2735db42b6eec9000c0b2a1ad1c4d39f1e25
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-25 00:21:07 -0400

    commit tree eace0f3abe1b9ee9c4fb1be199646b0d83a5ce2f

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,16 @@ class Timer extends Component {
   }
 
   //Your code here
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate (nextProps,nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    } 
+    return true
+  }
 
   componentDidMount() {
     this.interval = setInterval(
@@ -24,6 +34,7 @@ class Timer extends Component {
   }
 
   render() {
+    //console.log(this.timer.current)
     const { time, color, logText } = this.state;
     return (
       <section className="Timer" style={{ background: color }} ref={this.timer}>
